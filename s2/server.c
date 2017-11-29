bd791010aeae995b14e4d577f6abfe35a2d8689e
Marc <marc.sp@coditramuntana.com>
2017-11-29 18:11:01 +0100

    Servidor i client amb el while

--- s2/server.c
+++ s2/server.c
@@ -9,78 +9,77 @@
 
 
 int main(int argc, char* argv[]) {
-	
-	int sock, i; 
-	int bllegit, bescrit; 
-    char buff[200];
-	int ladrrem; 
-	struct sockaddr_in adrloc, adrrem; 
-	char iploc[16]; 
-	int portloc; 
-    
 
-    
-	
-	/* Es crea el socket UDP sock del servidor (el socket "local"), que de moment no té     */ 
-	/* adreça (@IP i #port UDP) assignada.                                                  */ 
-	if((sock=socket(AF_INET,SOCK_DGRAM,0))==-1) 
-	{ 
-		  perror("Error en socket"); 
-		  exit(-1); 
-	} 
-	 
-	/* Per assignar adreça a sock (@IP i #port UDP) el programador ho pot fer de 2 maneres: */ 
-	/* i) explícitament, és a dir, assignant uns valors concrets en el codi, fent bind()    */ 
-	/* amb aquests valors; ii) implícitament, és a dir, deixant que els assigni el S.O.,    */ 
-	/* que escull una @IP qualsevol de la màquina i un #port UDP que estigui lliure. Una    */ 
-	/* assignació implicita es pot fer d'1 manera: fent bind() amb @IP 0.0.0.0 (que vol dir */ 
-	/* qualsevol, és a dir, totes les @IP de la màquina) i #port UDP 0 (llavors, després    */ 
-	/* del bind(), el S.O. haurà assignat un #port UDP). Assignar l'@IP 0.0.0.0 vol dir     */ 
-	/* que el socket queda lligat a totes les @IP de la màquina i que rebrà paquets que     */ 
-	/* arribin a qualsevol d'elles.                                                         */ 
-	/* Aquí es fa bind amb @IP 0.0.0.0 i #port TCP 3000. 
-	*/ 
-	
-	strcpy(iploc,"0.0.0.0");    /* 0.0.0.0 correspon a INADDR_ANY */ 
-	portloc = 3000; 
-	adrloc.sin_family=AF_INET; 
-	adrloc.sin_port=htons(portloc); 
+	int sock, i;
+	int bllegit = 1, bescrit;
+    char buff[200];
+	int ladrrem;
+	struct sockaddr_in adrloc, adrrem;
+	char iploc[16];
+	int portloc;
+
+	for(i = 0; i < sizeof(buff); i++){
+		buff[i] = '\0';
+	}
+
+
+	/* Es crea el socket UDP sock del servidor (el socket "local"), que de moment no té     */
+	/* adreça (@IP i #port UDP) assignada.                                                  */
+	if((sock=socket(AF_INET,SOCK_DGRAM,0))==-1)
+	{
+		  perror("Error en socket");
+		  exit(-1);
+	}
+
+	/* Per assignar adreça a sock (@IP i #port UDP) el programador ho pot fer de 2 maneres: */
+	/* i) explícitament, és a dir, assignant uns valors concrets en el codi, fent bind()    */
+	/* amb aquests valors; ii) implícitament, és a dir, deixant que els assigni el S.O.,    */
+	/* que escull una @IP qualsevol de la màquina i un #port UDP que estigui lliure. Una    */
+	/* assignació implicita es pot fer d'1 manera: fent bind() amb @IP 0.0.0.0 (que vol dir */
+	/* qualsevol, és a dir, totes les @IP de la màquina) i #port UDP 0 (llavors, després    */
+	/* del bind(), el S.O. haurà assignat un #port UDP). Assignar l'@IP 0.0.0.0 vol dir     */
+	/* que el socket queda lligat a totes les @IP de la màquina i que rebrà paquets que     */
+	/* arribin a qualsevol d'elles.                                                         */
+	/* Aquí es fa bind amb @IP 0.0.0.0 i #port TCP 3000.
+	*/
+
+	strcpy(iploc,"0.0.0.0");    /* 0.0.0.0 correspon a INADDR_ANY */
+	portloc = 3000;
+	adrloc.sin_family=AF_INET;
+	adrloc.sin_port=htons(portloc);
 	adrloc.sin_addr.s_addr=inet_addr(iploc);    /* o bé: ...s_addr = INADDR_ANY */
 	for(i=0;i<8;i++){
 		adrloc.sin_zero[i]='\0';
-	} 
-	
-	//bind port del server
-	if((bind(sock,(struct sockaddr*)&adrloc,sizeof(adrloc)))==-1) 
-	{ 
-		perror("Error en bind"); 
-		close(sock); 
-		exit(-1); 
-	} 
-	
-	
-	/* Via sock es rep una línia del socket del client (el socket "remot").                 */ 
-	ladrrem=sizeof(adrrem); // longitud del adrrem
-	
-	
-	if((bllegit=recvfrom(sock,buff,sizeof(buff),0,(struct sockaddr*)&adrrem,&ladrrem))==-1) 
-	{ 
-		perror("Error recvfrom\n"); 
-		close(sock); 
-		exit(-1); 
-	} 
-	/* Ara adrrem conté l'adreça del socket remot (@IP i #port UDP).                        */ 
-	/* S'escriu a pantalla la línia que ha arribat.                                         */ 
-	
-	if((bescrit=write(1,buff,bllegit))==-1) 
-	{ 
-		perror("Error en write"); 
-		close(sock); 
-		exit(-1); 
 	}
 
-    /* Es tanca el socket sock.                                                             */ 
-	close(sock); 
-	
+	//bind port del server
+	if((bind(sock,(struct sockaddr*)&adrloc,sizeof(adrloc)))==-1)
+	{
+		perror("Error en bind");
+		close(sock);
+		exit(-1);
+	}
+
+
+	/* Via sock es rep una línia del socket del client (el socket "remot").                 */
+	ladrrem=sizeof(adrrem); // longitud del adrrem
+
+	while(bllegit > 0 && strcmp(buff, "$") != 0){
+		memset(buff,0,strlen(buff));
+
+		if((bllegit=recvfrom(sock,buff,sizeof(buff),0,(struct sockaddr*)&adrrem,&ladrrem))==-1)
+		{
+			perror("Error recvfrom\n");
+			close(sock);
+			exit(-1);
+		}
+		if(strcmp(buff, "$") != 0) printf("%s\n", buff);
+
+	}
+
+
+    /* Es tanca el socket sock.                                                             */
+	close(sock);
+
     return 0;
 }
